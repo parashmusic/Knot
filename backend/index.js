c63127ad74b9e3547da1bc5e1208f60994453730
parash <parashmusic770@gmail.com>
2025-10-09 18:42:03 +0530

    Update database configuration and project metadata

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,8 @@ const app = express()
 const server = createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 })
